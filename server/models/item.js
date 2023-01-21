faa92993f4a279b0be601e31b3b3f5f22e52d934
webDevAndres <macias.andres02@gmail.com>
2023-01-20 20:49:26 -0600

    add comments

--- server/models/item.js
+++ server/models/item.js
@@ -1,11 +1,18 @@
+/*
+============================================
+ Title: item.js
+ Author: Andres Macias
+ Date:   01/16/23
+ Description: create Item schema to be used in employee model
+===========================================
+*/
+
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 let itemSchema = new Schema({
-  text: { type: String },
-  todo: [Item],
-  done: [Item]
+  text: { type: String }
 })
 
-module.exports = mongoose.model('Item', itemSchema);
+module.exports = itemSchema;
